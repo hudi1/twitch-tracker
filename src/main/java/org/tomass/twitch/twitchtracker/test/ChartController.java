d6f74eb8f6a57eaa4ae7af77423fd8de3db2c3c8
tomas.hudec@i.cz <hudi1star@gmail.com>
2024-01-30 16:06:37 +0100

    grafy WIP

--- src/main/java/org/tomass/twitch/twitchtracker/test/ChartController.java
+++ src/main/java/org/tomass/twitch/twitchtracker/test/ChartController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.tomass.twitch.twitchtracker.model.TwitchStream;
@@ -18,8 +19,21 @@ public class ChartController {
     @Autowired
     private TwitchStreamService twitchStreamService;
 
-    @GetMapping("/data")
-    public List<ChartData> getChartData() {
+    @GetMapping("/game/{gameId}")
+    public List<ChartData> getChartData(@PathVariable Long gameId) {
+        System.out.println(gameId);
+        // System.err.println(gameId);
+        TwitchStream stream = twitchStreamService.getTwitchStream(gameId);
+        List<ChartData> result = new ArrayList<>();
+        for (TwitchStreamData data : stream.getTwitchStreamDatas()) {
+            result.add(new ChartData(data.getTs(), data.getViewerCount()));
+        }
+
+        return result;
+    }
+
+    @GetMapping("/game")
+    public List<ChartData> getChartData1() {
         TwitchStream stream = twitchStreamService.getTwitchStream(41796575320l);
         List<ChartData> result = new ArrayList<>();
         for (TwitchStreamData data : stream.getTwitchStreamDatas()) {
